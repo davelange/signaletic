e315cc2124d5f739f19a6836225e34ba720cfb02
David Lange <lange@finiam.com>
2025-03-29 14:48:30 +0000

    fix live scene form

--- apps/admin/src/modules/live/LiveSceneForm.svelte
+++ apps/admin/src/modules/live/LiveSceneForm.svelte
@@ -11,14 +11,18 @@
   import { routes } from '$lib/routes';
   import AddElement from '$modules/planner/AddElement.svelte';
   import ElementsPreview from '$modules/planner/ElementsPreview.svelte';
+  import PresetOptions from '$modules/planner/PresetOptions.svelte';
 
   const VISUALIZER_URL = import.meta.env.VITE_VISUALIZER_URL;
 
   let {
     project,
     scene: inputScene = $bindable()
-  }: { project: Project; scene?: DisplayScene } = $props();
-
+  }: {
+    project: Project;
+    scene?: DisplayScene;
+  } = $props();
+  let iframeEl = $state() as HTMLIFrameElement;
   const dialog = useDialog();
 
   let scene =
@@ -30,8 +34,10 @@
       displayId: project.displays.at(0)!.id
     });
 
-  let elements: DisplayScene['elements'] = $state([]);
-  let templateConfig: DisplayScene['templateConfig'] = $state({});
+  let elements = $state(scene.elements || []);
+  let templateConfig: DisplayScene['templateConfig'] = $state(
+    scene.templateConfig || {}
+  );
 
   let mutation = useMutation({
     fn: () =>
@@ -64,7 +70,6 @@
     }
 
     templateConfig = event.data.value.templateConfig || {};
-    elements = event.data.value.elements || [];
   }
 
   async function handleSubmit(e: SubmitEvent) {
@@ -76,39 +81,53 @@
 </script>
 
 <svelte:window onmessage={handleMessage} />
-<form class="flex w-full flex-col gap-4" onsubmit={handleSubmit}>
-  <div class="flex flex-1 gap-4">
-    <div class="w-full flex-1">
-      <Input
-        label="Name"
-        type="text"
-        name="name"
-        placeholder="Scene name"
-        required
-        bind:value={scene.name}
-      />
-    </div>
+<form
+  class="grid w-[90vw] grid-cols-4 items-start gap-4"
+  onsubmit={handleSubmit}
+>
+  <div class="col-span-2">
+    <Input
+      label="Name"
+      type="text"
+      name="name"
+      placeholder="Scene name"
+      required
+      bind:value={scene.name}
+    />
+  </div>
+
+  <div class="col-span-2">
     <Select
       label="Display"
       options={displays}
       name="displayId"
       bind:value={scene.displayId}
     />
+  </div>
+
+  <div class="col-span-2 flex">
+    <AddElement bind:elements />
+  </div>
+  <div class="col-span-2 flex items-end gap-4">
     <Select
       label="Template"
       options={templateOptions}
       name="templateId"
       bind:value={scene.templateId}
     />
-  </div>
-  <div class="flex">
-    <AddElement bind:elements />
+    <PresetOptions
+      bind:scene
+      {iframeEl}
+      currentConfig={templateConfig}
+      bind:currentElements={elements}
+    />
   </div>
   <div
-    class="relative flex aspect-video w-[90vw] max-w-full items-center justify-center bg-slate-100"
+    class="relative col-span-4 flex aspect-video w-[90vw] max-w-full items-center justify-center rounded-md"
   >
     {#if scene.templateId}
       <iframe
+        bind:this={iframeEl}
         src={routes.SCENE_TEMPLATE(scene.templateId, scene.id)}
         frameborder="0"
         title="Preview"
